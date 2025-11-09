ac1644d2beef3af1d05a672ed98c8fe0c0397777
nagimasen <nagimasensushi@gamil.com>
2025-11-09 21:16:14 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sales-tracker-1.0/sw.js', { scope: '/sales-tracker-1.0/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
